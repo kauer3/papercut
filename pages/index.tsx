73e42468a1a217d06737b0c5aab6ec3b53d29710
Kauê Reis <kaue0607@gmail.com>
2024-01-21 03:08:54 +1300

    Update index component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
-import Head from 'next/head'
-import styled from 'styled-components'
-import {
-  Container,
-} from '../components/sharedstyles'
-import Mountain from '../components/mountain'
-import Tree from '../components/tree'
-import Clouds from '../components/clouds'
-import Fire from '../components/fire'
-import House from '../components/house'
+import Head from "next/head";
+import styled from "styled-components";
+import { Container } from "../components/sharedstyles";
+import Mountain from "../components/mountain";
+import Tree from "../components/tree";
+import Clouds from "../components/clouds";
+import Fire from "../components/fire";
+import House from "../components/house";
 
 const ArtContainer = styled.div`
   position: relative;
@@ -29,9 +27,10 @@ const ArtContainer = styled.div`
     justify-content: space-between;
     padding-left: 50%;
   }
-`
+`;
 
-const Moon = styled.div` position: absolute;
+const Moon = styled.div`
+  position: absolute;
   align-self: center;
   transform: translate(-100%);
   background: #d0d0d0;
@@ -40,43 +39,57 @@ const Moon = styled.div` position: absolute;
   border-radius: 50%;
   background-color: #ffffff;
   opacity: 1;
-  background-image: radial-gradient(circle at center center, #fafafa, #ffffff), repeating-radial-gradient(circle at center center, #fafafa, #fafafa, 4px, transparent 8px, transparent 4px);
+  background-image: radial-gradient(circle at center center, #fafafa, #ffffff),
+    repeating-radial-gradient(
+      circle at center center,
+      #fafafa,
+      #fafafa,
+      4px,
+      transparent 8px,
+      transparent 4px
+    );
   background-blend-mode: multiply;
   box-shadow: 0 0 140px #ffffff;
   filter: drop-shadow(0 0 30px #ffe695);
-`
+`;
 
 const Ground = styled.div`
   position: relative;
   align-self: center;
-  background: #C2B280;
+  background: #c2b280;
   width: 120vw;
   height: 15vh;
   bottom: 0;
   /* left: 0; */
   z-index: 10;
   border-radius: 25% 30% 0% 0%;
-`
+`;
 
 export default function Home() {
   return (
     <Container>
       <Head>
-        <title>CSS Paper Art</title>
+        <title>Papercut - CSS Paper Art</title>
         <meta name="description" content="Paper Art created with css" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <ArtContainer >
+      <ArtContainer>
         <Moon />
         <Clouds />
-        {Array(4).fill(true).map((_, i) => <Mountain order={i} key={i} />)}
-        {Array(6).fill(true).map((_, i) => <Tree order={i} key={i} />)}
-        <div className="groud-elements-container">
-          <Fire />
-          <House />
-        </div>
-      </ArtContainer >
+        {Array(4)
+          .fill(true)
+          .map((_, i) => (
+            <Mountain order={i} key={i} />
+          ))}
+        {Array(6)
+          .fill(true)
+          .map((_, i) => (
+            <Tree order={i} key={i} />
+          ))}
+        <Fire />
+        <House />
+      </ArtContainer>
       <Ground />
     </Container>
-  )
+  );
 }
